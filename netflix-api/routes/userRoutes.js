8a11336d22909ad1af5ba0a01787b6c613199d05
SunmeetSunns <98201485+SunmeetSunns@users.noreply.github.com>
2024-04-04 20:41:14 +0530

    Revert "al commit"
    This reverts commit b59a93780344c6c4582e2f1085936c284bdff167.

--- netflix-api/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const { addToLiked, getLikedMovies, removeFromLikeMovies } = require("../controllers/userController");
-
-const router=require("express").Router();
-
-router.post("/add",addToLiked);
-router.get("/liked/:email",getLikedMovies);
-router.put("/delete",removeFromLikeMovies);
-module.exports=router;
